5d269e00f57e55d983664634558fb18b16702cf3
Jaden <jaden.anthony2@gmail.com>
2025-10-17 14:52:17 -0500

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,7 +10,7 @@ document.addEventListener(`DOMContentLoaded`, function() {
   gridCells.forEach(square => square.classList.add(`square`));
   
   const winCombinations = [
-    [0, 1, 2].
+    [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
