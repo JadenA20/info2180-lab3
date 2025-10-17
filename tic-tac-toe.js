4cf9540b7da5d81b690c41eff38e30e041f478d4
Jaden <jaden.anthony2@gmail.com>
2025-10-17 15:39:06 -0500

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -23,14 +23,14 @@ document.addEventListener(`DOMContentLoaded`, function() {
 
 let cellOptions = Array(9).fill(null);
 
-const playerX = 'X';
-const playerO = 'O';
+const playerX = "X";
+const playerO = "O";
 
 let firstPlayer = playerX;
 
 
 function setUpGame(){
-  gridCells = forEach((square, cell) => {
+  gridCells.forEach((square, cell) => {
     
     square.addEventListener(`hoverOn`, function(e) {
         e.target.classList.add(`hover`);
